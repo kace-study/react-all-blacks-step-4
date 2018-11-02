51c36f1413b256a729a0799c3c7f2ba11ccae7fb
kace-study <kace.study@gmail.com>
2018-11-02 12:03:42 +0900

    step4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PanelList from './components/PanelList/PanelList';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isPlaying: true }
+  }
+
+  endGame = () => {
+    this.setState({ isPlaying: false });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          { this.state.isPlaying ? 
+            <h1>ALL BLACKS</h1> :
+            <h1>Congrats! Haka!</h1>
+          }
+          <button>Start</button>
         </header>
+        <PanelList 
+          endGame={this.endGame} 
+          isPlaying={this.state.isPlaying}/>
       </div>
     );
   }
